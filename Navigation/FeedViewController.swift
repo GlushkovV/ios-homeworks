cc05fc5f6683a11e006011675b4d206cbc2284ec
Vitaly Glushkov <vit8@live.ru>
2022-03-02 21:38:45 +0400

    Выполнено ДЗ 4

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -8,14 +8,64 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .systemBackground
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var oneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Первая кнопка", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var twoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Вторая кнопка", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
         title = "Лента"
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(oneButton)
+        stackView.addArrangedSubview(twoButton)
+        let centerYStackViewConstraint = self.stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        let leadingStackViewConstraint = self.stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10)
+        let trailingStackViewConstraint = self.stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10)
+        let heightStackViewConstraint = self.stackView.heightAnchor.constraint(equalToConstant: 110)
+        NSLayoutConstraint.activate([
+            centerYStackViewConstraint, leadingStackViewConstraint, trailingStackViewConstraint,
+            heightStackViewConstraint
+        ])
+    }
+        /*
         let button = UIButton(frame: CGRect(x: 130, y: 770, width: 150, height: 40))
-        view.addSubview(button)
         button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 12
         button.setTitle("Перейти на сообщение", for: .normal)
@@ -26,6 +76,7 @@ class FeedViewController: UIViewController {
         button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150).isActive = true
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
+    */
     
     @objc func buttonClicked() {
         let postViewController = PostViewController()
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    private lazy var profileHeaderView: ProfileHeaderView = {
+    lazy var profileHeaderView: ProfileHeaderView = {
         let view = ProfileHeaderView(frame: .zero)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .lightGray
@@ -17,23 +17,45 @@ class ProfileViewController: UIViewController {
         return view
     }()
 
+    private lazy var newButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Новая кнопка", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowOpacity = 0.7
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var heightConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = false
         title = "Профиль"
-        view.addSubview(profileHeaderView)
         self.setupView()
+        
     }
     
     private func setupView() {
         self.view.backgroundColor = .systemBackground
         self.view.addSubview(self.profileHeaderView)
+        self.view.addSubview(self.newButton)
         let topConstraint = self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
-        let leadingConstraint = self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
-        let trailingConstraint = self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
-        let bottomConstraint = self.profileHeaderView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        let leadingConstraint = self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
+        let trailingConstraint = self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        self.heightConstraint = self.profileHeaderView.heightAnchor.constraint(equalToConstant: 245)
+        let bottomNewButtonConstraint = self.newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        let leadingNewButtonConstraint = self.newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16)
+        let trailingNewButtonConstraint = self.newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16)
+        let heightNewButtonConstraint = self.newButton.heightAnchor.constraint(equalToConstant: 50)
         NSLayoutConstraint.activate([
-            topConstraint, leadingConstraint, trailingConstraint, bottomConstraint
+            topConstraint, leadingConstraint, trailingConstraint, self.heightConstraint,
+            bottomNewButtonConstraint, leadingNewButtonConstraint, trailingNewButtonConstraint,
+            heightNewButtonConstraint
         ].compactMap({ $0 }))
     }
 }
@@ -41,8 +63,7 @@ class ProfileViewController: UIViewController {
 extension ProfileViewController: ProfileHeaderViewProtocol {
     
     func didTapShowStatusButton(textFieldIsVisible: Bool, completion: @escaping () -> Void) {
-        //self.heightConstraint?.constant = textFieldIsVisible ? 214 : 170
-                
+        self.heightConstraint?.constant = textFieldIsVisible ? 300 : 245
         UIView.animate(withDuration: 0.3, delay: 0.1) {
             self.view.layoutIfNeeded()
         } completion: { _ in
